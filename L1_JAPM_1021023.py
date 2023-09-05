3d4752a1dd90ab05c7ba1db5525066a6e74b9fb5
JAPMpaz <144158249+JAPMpaz@users.noreply.github.com>
2023-09-05 12:25:44 -0600

    Add files via upload

---
+++ L1_JAPM_1021023.py
@@ -0,0 +1,22 @@
+#introduccion a la programacion - seccion 17
+#05/09/2023
+#Josè Antonio Paz Marìn
+#objetivo:crear uin programa que diga "mi primer programa" y definir dos variables para operarlas
+#entrada:N1,N2
+"""
+procesos:
+1. solicitar al usario dos valores numericos.
+2. realizar la operaciones matematicas basicas con los dos numeros ingresados.
+3. mostrar todos los resultados.
+"""
+#salidas: resultadsos de las operaciones basicas
+print("Mi primer progrma")
+N1= input("NUMERO1: ")
+N2=input ("numero2: ")
+N1=int(N1)
+N2=int (N2)
+print("N1 + N2")
+print (N1 + N2)
+print(N1-N2)
+print (N1 * N2)
+print(N1/N2)
