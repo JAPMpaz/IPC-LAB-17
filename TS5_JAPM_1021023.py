2264c58e2cf9c405ade3e1883e96bed34e54bce2
JAPMpaz <144158249+JAPMpaz@users.noreply.github.com>
2023-09-07 09:24:53 -0600

    Add files via upload

---
+++ TS5_JAPM_1021023.py
@@ -0,0 +1,24 @@
+#Introduccion a la programacion-seccion 17
+#07/09/2023
+#Jose Antonio Paz Marín
+#entrada: nombre, edad, carrera, carne
+"""
+Proceso:
+solicitar nombre, edad, carrera y universidad en la que estudi el usuario
+mostrar la combinacion de toda la informacion que se solicito con anterioridad
+"""
+"""
+salida:
+informacion generla del usuario
+confirmacion de informacion ingresada
+"""
+print("Mi Segundo Porgrama")
+sNombre=str(input("Nombre:")) 
+sEdad=str (input ("edad:"))
+sCarrera=str(input("carrera:" ))
+sCarne=str(input("carne:" ))
+print("nombre:",sNombre)
+print("edad:",sEdad)
+print("carrera:",sCarrera)
+print("carne:",sCarne)
+print("Mi nombre es",sNombre,"tengo",sEdad,"años","estudio",sCarrera,"mi carne es",sCarne)
